2e5f6c1b1677d2d913103e8fc7cdb19cb7cff92a
HelioLucio <helio.lucio.jr@hotmail.com>
2023-09-25 11:05:45 -0300

    changed logo to 'Instagram'

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -21,9 +21,10 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('dashboard')">
-                                    <ApplicationLogo
+                                    <!-- <ApplicationLogo
                                         class="block h-9 w-auto fill-current text-gray-800"
-                                    />
+                                    /> -->
+                                    Instagram
                                 </Link>
                             </div>
 
